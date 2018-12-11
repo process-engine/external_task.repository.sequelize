c2dfead32c466a3a091e5e46ad9cb9e42ffdafd9
ElRaptorus <christian.werner@5minds.de>
2018-12-11 09:32:20 +0100

    :lipstick: Improve some logging

--- src/external_task_repository.ts
+++ src/external_task_repository.ts
@@ -38,7 +38,7 @@ export class ExternalTaskRepository implements IExternalTaskRepository, IDisposa
     logger.verbose('Initializing Sequelize connection and loading models...');
     const connectionAlreadyEstablished: boolean = this._sequelize !== undefined;
     if (connectionAlreadyEstablished) {
-      logger.verbose('Repository already initialized.');
+      logger.verbose('Repository already initialized. Done.');
 
       return;
     }
@@ -51,7 +51,7 @@ export class ExternalTaskRepository implements IExternalTaskRepository, IDisposa
     logger.verbose('Disposing connection');
     await this._connectionManager.destroyConnection(this.config);
     this._sequelize = undefined;
-    logger.verbose('done');
+    logger.verbose('Done.');
   }
 
   public async create<TPayload>(topic: string,
